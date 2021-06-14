cb5139caf7c94c6fb4d15fbf892f2cb35cc967a1
Gustavo <gustavobrcn@hotmail.com>
2021-06-14 01:47:14 -0400

    check dupes function works but needs tidying up

--- download_organizer.py
+++ download_organizer.py
@@ -8,66 +8,99 @@ import win32con
 from win10toast_click import ToastNotifier
 
 
-path_to_watch = os.path.abspath ("C:/Users/gusta/Downloads")
+toast = ToastNotifier()
+path_to_watch = os.path.abspath("C:/Users/gusta/Downloads")
 
-#
+
 # FindFirstChangeNotification sets up a handle for watching
 #  file changes. The first parameter is the path to be
 #  watched; the second is a boolean indicating whether the
 #  directories underneath the one specified are to be watched;
 #  the third is a list of flags as to what kind of changes to
 #  watch for. We're just looking at file additions / deletions.
-#
+
 change_handle = win32file.FindFirstChangeNotification(
   path_to_watch,
   0,
   win32con.FILE_NOTIFY_CHANGE_FILE_NAME
 )
 
-# file types to look for
 
-def move_file_and_toast(file):
+def check_year_and_month_folder(des_folder):
+    # Check the directory for current month and year folders. If they do not exist, create them
+    curr_year, curr_month = datetime.datetime.now().strftime('%Y-%b').split('-')
+    des_folder += '/' + curr_year
+    if not os.path.exists(des_folder):
+        os.mkdir(des_folder)
+        des_folder += '/' + curr_month
+        os.mkdir(des_folder)
+       
+    else:
+        des_folder += '/' + curr_month
+        if not os.path.exists(des_folder):
+            os.mkdir(des_folder)
+    
+    return des_folder
+        
+
+def toast_and_open_folder(des_folder, file_type):
+    # Create windows toast for downloaded file
+    open_folder = lambda: os.startfile(des_folder)
+    file_types = {
+        'Picture': 'icons/pic.ico',
+        'Video': 'icons/vid.ico',
+        'Document': 'icons/doc.ico',
+        'Music': 'icons/music.ico',
+        'Download': 'icons/down.ico'
+    }
+    toast.show_toast(f'{file_type[:-1]} Type File Downloaded', f'Open the {file_type} Library to view the downloaded file.', duration=3, icon_path=file_types[file_type],  threaded=False, callback_on_click=open_folder)
+    
+def move_file(file):
     # Move the downloaded file to its proper destination folder and trigger toast
     doc_types = ['zip', 'txt', 'docx', 'pptx', 'xlsx', 'pdf']
     pic_types = ['png', 'jpg', 'jpeg', 'ico']
     vid_types = ['mov', 'mpg', 'mpeg', 'mp4', 'mpge4', 'avi']
     music_types = ['mp3', 'wav']
-    root = 'C:/Users/gusta/'
+    des_folder = 'C:/Users/gusta/'
+    file_to_move = path_to_watch + '/' + file
     file_type = file.split('.')[-1]
     print(file_type)
 
     if file_type in doc_types:
-        des_folder = root + 'Documents'
-        os.rename(f'{path_to_watch}/{file}', f'{des_folder}/{file}')
+        des_folder += 'Documents'
+        des_folder = check_year_and_month_folder(des_folder)
+        file_path = des_folder + '/' + file
+        os.rename(file_to_move, file_path)
+        toast_and_open_folder(des_folder, 'Document')
+        
         
     elif file_type in pic_types:
-        des_folder = root + 'Pictures'
-        os.rename(f'{path_to_watch}/{file}', f'{des_folder}/{file}')
+        des_folder += 'Pictures'
+        des_folder = check_year_and_month_folder(des_folder)
+        file_path = des_folder + '/' + file
+        os.rename(file_to_move, file_path)
+        toast_and_open_folder(des_folder, 'Picture')
         
     elif file_type in vid_types:
-        des_folder = root + 'Videos'
-        os.rename(f'{path_to_watch}/{file}', f'{des_folder}/{file}')
+        des_folder += 'Videos'
+        des_folder = check_year_and_month_folder(des_folder)
+        file_path = des_folder + '/' + file
+        os.rename(file_to_move, file_path)
+        toast_and_open_folder(des_folder, 'Video')
         
     elif file_type in music_types:
-        des_folder = root + 'Music'
-        os.rename(f'{path_to_watch}/{file}', f'{des_folder}/{file}')
+        des_folder += 'Music'
+        des_folder = check_year_and_month_folder(des_folder)
+        file_path = des_folder + '/' + file
+        os.rename(file_to_move, file_path)
+        toast_and_open_folder(des_folder, 'Music')
     
-def check_year_and_month_folder(des_folder):
-    curr_year, curr_month = datetime.datetime.now().strftime('%Y-%b').split('-')
-    dir = os.listdir(des_folder)
-    des_folder += '/' + curr_year
-    if curr_year not in dir:
-        os.mkdir(des_folder)
-        des_folder += '/' + curr_month
-        os.mkdir(des_folder)
-        return des_folder
-    else:
-        dir = os.listdir(des_folder)
-        des_folder += '/' + curr_month
-        if curr_month not in dir:
-            os.mkdir(des_folder)
-        return des_folder
-    
+    else: 
+        des_folder += 'Downloads'    
+        des_folder = check_year_and_month_folder(des_folder)
+        file_path = des_folder + '/' + file
+        os.rename(file_to_move, file_path)
+        toast_and_open_folder(des_folder, 'Downloads')
     
 
 # Loop forever, listing any file changes. The WaitFor... will
@@ -90,11 +123,13 @@ try:
       sleep(1) # to prevent "couldn't download" error in browser event though file was downloaded
       new_path_contents = dict ([(f, None) for f in os.listdir (path_to_watch)])
       added = [f for f in new_path_contents if not f in old_path_contents]
-      
-      if added: 
-          for f in added:
-            print(f)
-            move_file_and_toast(f)
+      print(added)
+      if added : 
+        for file in added:
+            if 'crdownload' in file:
+                continue
+            else:
+              move_file(file)
           
 
       old_path_contents = new_path_contents
--- test.py
+++ test.py
@@ -1,28 +1,76 @@
 import datetime
 import os
+from time import process_time_ns
+from win10toast_click import ToastNotifier
 
+toast = ToastNotifier()
 
 path = "C:/Users/gusta/Downloads"
 
 def check_year_and_month_folder(des_folder):
     curr_year, curr_month = datetime.datetime.now().strftime('%Y-%b').split('-')
-    dir = os.listdir(des_folder)
+    # dir = os.listdir(des_folder)
     des_folder += '/' + curr_year
-    if curr_year not in dir:
+    if not os.path.exists(des_folder):
         os.mkdir(des_folder)
         des_folder += '/' + curr_month
         os.mkdir(des_folder)
-        return des_folder
+       
     else:
-        dir = os.listdir(des_folder)
+        # dir = os.listdir(des_folder)
         des_folder += '/' + curr_month
-        if curr_month not in dir:
+        if not os.path.exists(des_folder):
             os.mkdir(des_folder)
-        return des_folder
+    return des_folder
         
-check_year_and_month_folder(path)
+# check_year_and_month_folder(path)
 
+def toast_and_open_folder(des_folder, file_type):
+    callback = lambda: os.startfile(des_folder)
+    file_types = {
+        'Picture': 'icons/pic.ico',
+        'Video': 'icons/vid.ico',
+        'Document': 'icons/doc.ico',
+        'Music': 'icons/music.ico'
+    }
+    toast.show_toast(f'{file_type} Type File Downloaded', f'Open the {file_type} Library to view the downloaded file.', duration=3, icon_path=file_types[file_type],  threaded=False, callback_on_click=callback)
+
+# toast_and_open_folder(path, 'Picture')
 # Keep changing value of des_folder
 # curr_year, curr_month = datetime.datetime.now().strftime('%Y-%b').split('-')
 
 # os.mkdir(path+'/'+ curr_year)
+
+def check_for_dupes(des_folder, file):
+    file_path = des_folder + '/' + file
+    file_exists = os.path.isfile(file_path)
+    print(file_path, file_exists)
+    file_name = file[:-4] # filename is still the same
+    file_type = file[-4:]
+    while file_exists:
+        print('beginning of loop')
+        file_num = 1
+        last_2 = file_name[-2:] # this needs a change
+        print(file_name, file_type, last_2)
+        # break
+        if last_2[0] == '_' and last_2[-1].isdigit():
+            print('if block')
+            file_name = file_name[:-1] + str(int(last_2[-1]) + 1) 
+            
+            
+        else:
+            file_name = file_name + '_' + str(file_num) 
+            file_num += 1
+            print('in else block')
+            print(file_name)
+            # break
+           
+        new_file_path = des_folder + '/' + file_name + file_type
+        print('new file path: ' + new_file_path)
+        file_exists = os.path.isfile(new_file_path)
+        print(file_exists, new_file_path)
+        print('end of loop')
+    return new_file_path
+        
+os.path.exists
+check_for_dupes(path + '/New Folder', 'New Text Document.txt')
